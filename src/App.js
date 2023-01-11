0bcee3b1e3272a89c97efbbe72b88b1999f0a8c0
Ar Kar Bo <eia99fia99@gmail.com>
2023-01-11 16:53:29 +0630

    deploy with Dockerfile,docker-compose.yml,nginx.conf

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
 import './App.css';
 import Title from './Title';
-import ListCards from './ListCards';
+import Card from "./Card";
 import { useState } from 'react';
 function App() {    
-  const [numbers,setNumbers] = useState(["One","Two","Three","Four","Five","Six"]);    
+  const [cards,setCards] = useState([]);    
 
-  const shuffle = (arr) => {
-    let last_index = arr.length - 1;
-    while(last_index > 0) {
-      let random_index = Math.floor(Math.random() * last_index);
-      let temp = arr[last_index];
-      arr[last_index] = arr[random_index];
-      arr[random_index] = temp;
-      last_index--;
-    }    
-    setNumbers([...arr])               
-  }   
-  
+  // const shuffle = (arr) => {
+  //   let last_index = arr.length - 1;
+  //   while(last_index > 0) {
+  //     let random_index = Math.floor(Math.random() * last_index);
+  //     let temp = arr[last_index];
+  //     arr[last_index] = arr[random_index];
+  //     arr[random_index] = temp;
+  //     last_index--;
+  //   }    
+  //   setCards([...arr])               
+  // }   
+  const addNewCard = () => {
+    setCards([...cards,{
+      id: Math.floor(Math.random() * 1000),
+      title: "This is Card Title",
+      description: "This is a card description."
+    }])
+  }
+  const deleteCard = (id) => {                       
+    setCards(cards.filter(card => card.id !== id))
+  }
   
   return (
     <div className="App">
         <Title/> 
                                 
-        <button onClick={()=>{shuffle(numbers);}}>Shuffle</button>
-        <ListCards numbers={numbers}/>
+        {/* <button onClick={()=>{shuffle(cards);}}>Shuffle</button> */}
+        <input type="text"></input>
+        <button onClick={()=>{addNewCard("Seven")}}>Add Card</button>  
+        <div className='ListCards'>
+        {
+          cards.length > 0 ? cards.map((card) => <Card id={card.id} title={card.title} description={card.description} deleteCard={deleteCard} key={card.id}/>):
+          <h4>There is no card</h4>
+        }
+        </div>      
+        
 
     </div>
   );
--- src/Card.js
+++ src/Card.js
@@ -1,22 +1,12 @@
 import React from "react";
-import './Card.css'
-import { useState } from 'react';
 
-const Card = ({number}) => {    
-    const [check, setCheck] = useState(false);
-    const [color, setColor] = useState("Close");
-    const [label, setLabel] = useState("Blank")
-    console.log(check + " "+ number);
-    const className = "Card " + color;
-    const btnClick = (msg)=>{
-        console.log(msg + " " + number)
-        setCheck(!check)
-        setColor(color==="Open"?"Close":"Open")
-        setLabel(label==="Blank"?number:"Blank")
-    }
+const Card = ({id,title,description,deleteCard}) => {                    
+    
     return (        
-        <div className={className} onClick={()=>{btnClick("Clicked")}} >
-            <h1>{label}</h1>                   
+        <div className="Card">
+            <h1>{title}</h1>                  
+            <p>{description}</p>
+            <button  onClick={()=>{deleteCard(id)}} >Delete</button>
         </div> 
     );
 }
